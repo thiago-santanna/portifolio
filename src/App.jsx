6a686713aeae42d3078d146bb48d070ea4d0b01a
thiago-santanna <uzmaki_thiago@outlook.com>
2022-07-04 14:43:54 -0300

    primeira versao do menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { HeaderNavBar } from "./components/header";
 import "./global.css";
 
 export function App() {
   return (
     <>
+      <HeaderNavBar />
       <h1>HomePage</h1>
     </>
   );
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,26 @@
-export function Header() {
+import logo from "../../public/logo.svg";
+import styles from "../../styles/HeaderNavBar.module.css";
+
+export function HeaderNavBar() {
   return (
     <>
-      <h1>Header</h1>
+      <div className={styles.container}>
+        <div className={styles.imageContainer}>
+          <img src={logo} alt="Logo Marga TSS Web Apps" />
+        </div>
+        <div className={styles.navBarContainer}>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Projetos</li>
+              <li>Blog</li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button>Deixe seu recado</button>
+        </div>
+      </div>
     </>
   );
 }
